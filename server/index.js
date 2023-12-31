98ab2e8ab93ac19a881c22df097397ab649df8bf
unknown <hsuntariq@gmail.com>
2023-12-31 16:17:27 +0500

    facebook

--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ connectDB()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/users/', require('./routes/userRoutes'))
+app.use('/api/users/', require('./routes/userRoutes'));
 
 app.listen(process.env.PORT, () => console.log(`Server started on port:${process.env.PORT.yellow}`))
